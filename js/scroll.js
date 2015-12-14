4fcde111b300944771397e296925a661a6774633
Lindsey <lindseydwhitney@gmail.com>
2015-12-14 16:21:16 -0700

    Added scroll.js to hide and reveal the nav bar based on scroll.

---
+++ js/scroll.js
@@ -0,0 +1,40 @@
+// Nav Hide/Reveal on Scroll -------------------------
+
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+var didScroll;
+
+// on scroll, let the interval function know the user has scrolled
+$(window).scroll(function(event){
+  didScroll = true;
+});
+// run hasScrolled() and reset didScroll status
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+function hasScrolled() {
+  var st = $(this).scrollTop();
+
+  if ( Math.abs(lastScrollTop - st) <= delta ) {
+  return;
+  }
+
+  // If current position > last position AND scrolled past navbar...
+  if (st > lastScrollTop){
+  // Scroll Down
+  $('nav').removeClass('nav-down').addClass('nav-up');
+} else {
+  // Scroll Up
+  // If did not scroll past the document (possible on mac)...
+  if(st + $(window).height() < $(document).height()) {
+    $('nav').removeClass('nav-up').addClass('nav-down');
+  }
+}
+
+lastScrollTop = st;
+
+}
