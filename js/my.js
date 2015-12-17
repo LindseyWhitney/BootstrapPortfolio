045c26c508828d626c49191e3db2474f286eded8
Lindsey <lindseydwhitney@gmail.com>
2015-12-17 13:15:05 -0700

    Minor aesthetic changes to nav and footer. Began install of smoothState.js

--- js/my.js
+++ js/my.js
@@ -222,3 +222,24 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+
+
+
+// Smooth scrolling on anchor tags -------------------------
+// Reference: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
