af9066f66611934ce6cce4444d028de00530db46
Lindsey <lindseydwhitney@gmail.com>
2016-03-16 09:01:54 -0600

    Found and fixed PHP error to remove blank page.

--- index.php
+++ index.php
@@ -383,6 +383,9 @@
           <div class="row">
 
             <?php
+
+            ini_set('display_errors',1);  error_reporting(E_ALL);
+
               if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
                 $email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
@@ -435,7 +438,7 @@
               }
 
             if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
-                echo "<p class="form_submission">Thanks for the message! I&rsquo;ll be in touch with you soon.</p>";
+                echo "<p class='form_submission'>Thanks for the message! I&rsquo;ll be in touch with you soon.</p>";
             } else {
                 if (isset($error_message)) {
                     echo "<p class='message'>".$error_message . "</p>";
@@ -443,23 +446,31 @@
             ?>
 
             <form method=“post” action=“index.php”>
-              <div class="col-md-6 col-md-offset-3 form-left">
+              <div class="col-md-6">
                 <div class="form-group">
-                  <label for="form__name" class="sr-only">Name</label>
-                  <input type="text" class="form-control" id="form__name" name="name" placeholder="Name (Required)" value="<?php if (isset($name)) { echo $name; } ?>">
+                  <label for="form__fname" class="sr-only">First Name</label>
+                  <input type="text" class="form-control" id="form__fname" name="name" placeholder="First Name (Required)" value="<?php if (isset($name)) { echo $name; } ?>">
+                </div>
+                <div class="form-group">
+                  <label for="form__lname" class="sr-only">Last Name</label>
+                  <input type="text" class="form-control" id="form__lname" name="name" placeholder="Last Name (Required)" value="<?php if (isset($name)) { echo $name; } ?>">
                 </div>
                 <div class="form-group">
                   <label for="form__email" class="sr-only">Email</label>
                   <input type="email" class="form-control" id="form__email" name="mail" placeholder="Email (Required)" value="<?php if (isset($email)) { echo $email; } ?>">
                 </div>
                 <div class="form-group">
+                  <label for="form__phone" class="sr-only">Phone Number</label>
+                  <input type="text" class="form-control" id="form__phone" name="name" placeholder="Phone Number (Required)" value="<?php if (isset($name)) { echo $name; } ?>">
+                </div>
+                <div class="form-group">
                   <label for="form__marketing" class="sr-only">How did you hear about me?</label>
                   <input type="text" class="form-control" id="form__marketing" name="marketing" placeholder="How did you hear about me?" value="<?php if (isset($marketing)) { echo $marketing; } ?>">
                 </div>
               </div> <!-- .col-md-6 form-left -->
-              <div class="form-group col-md-6 col-md-offset-3">
+              <div class="form-group col-md-6">
                 <label for="form__message" class="sr-only">Message</label>
-                <textarea name="message" class="form-control" rows="6" id="form__message" placeholder="What's on your mind?"><?php if (isset($message)) { echo htmlspecialchars($_POST["message"]); } ?></textarea>
+                <textarea name="message" class="form-control" rows="11" id="form__message" placeholder="What's on your mind?"><?php if (isset($message)) { echo htmlspecialchars($_POST["message"]); } ?></textarea>
               </div>
               <div style="display:none">
                 <label for="form__address" class="sr-only">Address</label>
