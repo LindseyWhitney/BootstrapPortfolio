46a054dd9bd226335cbd4eaf7b17658fdb47c1de
Lindsey <lindseydwhitney@gmail.com>
2016-05-19 10:10:42 -0600

    Attempt to fix IE nav reveal, part 2

--- js/navreveal.js
+++ js/navreveal.js
@@ -1,6 +1,10 @@
 // Nav | Hide/Reveal Based on Scroll Location ========================================
 // Reference: http://stackoverflow.com/questions/13549214/make-a-div-appear-when-scrolling-past-a-certain-point-of-a-page
 
+$(document).ready(function() {
+  $('.navbar-fixed-top').hide();
+});
+
 
 window.addEventListener("scroll",function() {
 
@@ -11,7 +15,6 @@ window.addEventListener("scroll",function() {
    }
    else {
       $('.navbar-fixed-top').slideUp(1000);
-
       $('.navbar-fixed-top').addClass('nav-up');
    }
 },false);
